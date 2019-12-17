0e9551e4fcecfa720ed5e4690e50d820a3d1be85
MaximeC <chambaudm@K119-1-5.dep-info.iutmontp.univ-montp2.fr>
2019-12-17 16:46:20 +0100

    tets

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.json({"test":"oui"})
 })
 
 app.listen(PORT, function () {
