d51629dde6f4b1b7db37c3781344eb5aa1b0cfb4
Lucas <l.escavia@gmail.com>
2019-12-17 16:40:11 +0100

    heroku axios v2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
+
+app.use(cors())
 
 app.get('/', function (req, res) {
   res.json({test : "ok"})
