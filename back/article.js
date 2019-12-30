5e1d7990c9499855423a845c9c8814db6828fe34
Maxime C <maxime.chambaud@me.com>
2019-12-30 12:26:47 +0100

    debut crud

--- back/article.js
+++ back/article.js
@@ -1,9 +1,61 @@
 const db=require('./db.js')
 const axios = require('axios').default;
+const url=db.DB_ADDR+"article"
 
 async function getArticles()
 {
-	return await axios.get(db.DB_ADDR+"article",db.DB_HEADERS).then(resp => 
+	return await axios.get(url,db.DB_HEADERS).then(resp => 
+	{
+		return resp.data
+
+	}).catch(fail=>
+	{
+		return fail
+	})
+}
+
+async function getArticle(id)
+{
+	url+="/"+id
+	return await axios.get(url,db.DB_HEADERS).then(resp => 
+	{
+		return resp.data
+
+	}).catch(fail=>
+	{
+		return fail
+	})
+}
+
+async function insertArticle(article)
+{
+	return await axios.put(url,article,db.DB_HEADERS).then(resp => 
+	{
+		return resp.data
+
+	}).catch(fail=>
+	{
+		return fail
+	})
+}
+
+async function updateArticle(article)
+{
+	url+="/"+id
+	return await axios.update(url,article,db.DB_HEADERS).then(resp => 
+	{
+		return resp.data
+
+	}).catch(fail=>
+	{
+		return fail
+	})
+}
+
+async function deleteArticle(id)
+{
+	url+="/"+id
+	return await axios.delete(url,db.DB_HEADERS).then(resp => 
 	{
 		return resp.data
 
@@ -14,5 +66,9 @@ async function getArticles()
 }
 
 module.exports = {
-  getArticles: getArticles
+  getArticles: getArticles,
+  getArticle:getArticle,
+  insertArticle: insertArticle,
+  updateArticle:updateArticle,
+  deleteArticle: deleteArticle
 }
--- index.js
+++ index.js
@@ -3,21 +3,42 @@ const cors = require('cors')
 const axios = require('axios')
 const article = require('./back/article.js')
 const app = express()
-const PORT = process.env.PORT || 5000 // this is very important
-
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 
 app.get('/', function (req, res) {
-	res.json({test : "ok"})
+	res.send("<h1>Bienvenue sur le blog de NodeVueJs !!! </h1>")
 })
 
-app.get('/lesArticles', async function (req, res) {
+app.get('/getArticles', async function (req, res) {
 	const articles = await article.getArticles()
+	res.send(articles)
+})
+
+app.get('/getArticle/:id', async function (req, res) {
+	const articles = await article.getOneArticleById(req.params.id)
+	res.send(articles)
+})
+
+app.get('/insertArticle', async function (req, res) {
+	//article = ?
+	const articles = await article.deleteArticle(article)
+	res.send(articles)
+})
+
+app.get('/updateArticle/:id', async function (req, res) {
+	//article = ?
+	const articles = await article.deleteArticle(req.params.id,article)
+	res.send(articles)
+})
+
+app.get('/deleteArticle/:id', async function (req, res) {
+	const articles = await article.deleteArticle(req.params.id)
 	res.send(articles)
 })
 
 
 app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT)
+  console.log('J écoute sur le port suivant :  ' + PORT)
 })
