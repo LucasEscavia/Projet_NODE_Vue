a78e03c96952a4ecfce9ab2f57403719ae294f3e
Maxime CHAMBAUD <38077678+maximeCHAMBAUD@users.noreply.github.com>
2020-03-04 17:09:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ passport.use(JwtStrategy)
 
 app.use(cors())
 
+app.options('/*', function (request, response, next) {
+	response.header("Access-Control-Allow-Origin", "*");
+    	response.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    	response.send();
+});
 app.get('/', function (req, res) {
 	res.status(200).json({ message: '<h1>Bienvenue sur le blog de NodeVueJs !!! </h1>' })
 })
